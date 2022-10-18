e2b04d27ffd463bf6aeed59329783c23bbef743c
deepika <dpka.kavita@gmail.com>
2022-10-18 20:14:03 +0530

    PDP Design Test

---
+++ app/design/frontend/Mycompany/theme/registration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Copyright © mycompany, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ * Author dpka.kavita@gmail.com
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/MyCompany/theme', __DIR__);
+
